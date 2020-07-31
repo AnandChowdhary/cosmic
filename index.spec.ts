14478479e48f8452e21f80b5b607a6a78e889fd6
Anand Chowdhary <mail@anandchowdhary.com>
2020-07-31 16:24:47 +0530

    :white_check_mark: Add .rc file test

--- index.spec.ts
+++ index.spec.ts
@@ -1,4 +1,12 @@
 import { cosmic, cosmicSync, config, clearCosmicCache } from "./index";
+import fs from "fs";
+
+beforeAll(async () => {
+  await fs.promises.writeFile(".cosmicrc", JSON.stringify({ rcValue: "test" }));
+});
+afterAll(async () => {
+  await fs.promises.unlink(".cosmicrc");
+});
 
 describe("environment variables", () => {
   it("has env async", async () => {
@@ -34,5 +42,16 @@ describe("clear cosmic cache", () => {
     expect(config("nodeEnv")).toBe("test");
     clearCosmicCache();
     expect(config("nodeEnv")).toBeUndefined();
+  });
+});
+
+describe("get config from an .rc file", () => {
+  it("has env async", async () => {
+    await cosmic("cosmic");
+    expect(config("rcValue")).toBe("test");
+  });
+  it("has env sync", () => {
+    cosmicSync("cosmic");
+    expect(config("rcValue")).toBe("test");
   });
 });
